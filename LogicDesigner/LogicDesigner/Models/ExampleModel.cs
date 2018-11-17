f0ff602ac88a4bae8cc820334b209697bdeb573a
DracoLight <45123811+DracoLight@users.noreply.github.com>
2018-11-17 14:46:47 +0100

    Initial upload of project files

---
+++ LogicDesigner/LogicDesigner/Models/ExampleModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDesigner.Models
+{
+    public class ExampleModel : BaseModel
+    {
+        // property can be created with 'propfull'+[Tab]+[Tab]
+        private int exampleInt = 0;
+
+        public int ExampleInt
+        {
+            get { return exampleInt; }
+            set
+            {
+                exampleInt = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string exampleString;
+
+        public string ExampleString
+        {
+            get { return exampleString; }
+            set
+            {
+                exampleString = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ LogicDesigner/LogicDesigner/ViewModels/icon.cs
@@ -0,0 +1,43 @@
+﻿using LogicDesigner.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDesigner.ViewModels
+{
+    public class icon : BaseModel
+    {
+
+        // Needs to be property to be bound to view
+        public ExampleModel Test { get; set; }
+        private string test2;
+
+        public string Test2
+        {
+            get { return test2; }
+            set {
+                test2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        // constructor
+        public icon ()
+        {
+            Test = new ExampleModel();
+            Test.ExampleString = "Hello World";
+        }
+
+        public void example_Click()
+        {
+            Test.ExampleInt += 1;
+        }
+
+
+    }
+}
